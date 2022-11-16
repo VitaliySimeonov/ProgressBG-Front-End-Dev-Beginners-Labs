db89c7c857078da1f2c45d463338f9ea60bd7760
WWWCourses <ivawebcourses@gmail.com>
2022-11-16 22:07:20 +0200

    Lab6: 16.11.2022 DONE: till http://wwwcourses.github.io/ProgressBG-Front-End-Dev/pages/themes/beginners/HTTP_Intro/HTTP_Intro.html#/9/2

---
+++ lab6/mysite.com/back-end/login.py
@@ -0,0 +1,8 @@
+# Connect to DB:
+
+
+# data: userName=Ada&pass=123
+# USER:
+
+# id | name| pass |
+#   1| Ada | 123  |
---
+++ lab6/mysite.com/back-end/registration.py
@@ -0,0 +1,7 @@
+# Connect to DB:
+
+# Insert data
+# USER:
+
+# id | name| pass |
+#   1| ada | 123  |
