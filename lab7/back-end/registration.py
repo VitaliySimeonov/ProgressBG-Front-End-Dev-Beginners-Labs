4f7eaba0daa7f4668a0591412ef9dd16534d074f
WWWCourses <ivawebcourses@gmail.com>
2022-11-21 22:02:19 +0200

    Lab7: 21.11.2022 DONE: HTML_InputForms

---
+++ lab7/back-end/registration.py
@@ -0,0 +1,8 @@
+# userName=ada&passWord=123&gender=male
+
+
+INSERT INTO USERS (name, pass) VALUES(userName, passWord)
+
+USERS:
+id | name | pass | gender | colors
+  1| ada  |  123 | male   | red;green
